bf1f0b2909d25a86b9ebca4839b9bebf0c12faf2
CHANHO <chanhokim9848@gmail.com>
2024-03-30 16:44:44 +0900

    30/03/2024

--- Greedy/Split With Minimum Sum.py
+++ Greedy/Split With Minimum Sum.py
@@ -1,3 +1,6 @@
-a=sorted("167331")
-
-print(int(''.join(a[::2]))+int(''.join(a[1::2])))
+class Solution:
+    def splitNum(self, num: int) -> int:
+        num="".join(sorted(str(num)))
+        return int(num[::2])+int(num[1::2])
+    
+print(Solution.splitNum(Solution, 4325))
