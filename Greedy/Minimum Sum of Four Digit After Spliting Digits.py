b9d893a99a3b8a2dc2302f85c1918a5e4337cedb
CHANHO <chanhokim9848@gmail.com>
2024-04-15 15:11:38 +0900

    15/04/2024

--- Greedy/Minimum Sum of Four Digit After Spliting Digits.py
+++ Greedy/Minimum Sum of Four Digit After Spliting Digits.py
@@ -1,27 +1,31 @@
-# This code snippet takes a number, converts it to a string,
-# sorts the digits in ascending order, and then performs a specific 
-# concatenation and summation operation on the sorted digits. Here's a breakdown:
 class Solution(object):
     def minimumSum(self, num):
+# Convert 'num' array to a string and sort it in ascending order.
+# Initialize pointers 'i' and 'j' at the start and end of the sorted list, respectively.
+# Iterate until 'i' exceeds 'j'.
+# 'res' holds the resulting value to be returned.
+# 'i' represents the minimum value in the array, and 'j' represents the maximum.
+# Concatenate and convert 'i' and 'j' to an integer value.
+# Add each integer value to 'res' while 'i' and 'j' move inward, repeating the process.
         num = sorted(str(num))
-        # This line converts the input number num into a string, sorts the digits of the string in ascending order, and stores the result back into the variable num.
         i, j = 0, len(num) - 1
-        # Initializes two pointers i and j to the start and end of the sorted num array respectively.
         res = 0
-        #Initializes a variable res to store the result of the summation.
-        #The while loop while i < j: iterates until i becomes greater than or equal to j.
-        res += int(num[i] + num[j])
-        # Inside the loop, 
-        #it concatenates the i-th digit of num with the j-th digit of num,
-        #converts the concatenated string into an integer, and adds it to res.
- #i += 1 and j -= 1: These lines increment i and decrement j to move towards the center of the sorted num array.
-
-# Finally, return res returns the accumulated result.
-# Overall, this code concatenates the smallest digit of the sorted num with the largest one,
-# the second smallest with the second largest, and so on, 
-# until it reaches the middle. It sums up these concatenated integers and returns the result.
+        while i<j:
+            res += int(num[i] + num[j])
+            i+=1
+            j-=1
+        return res
+    
+# Overall, the code sorts the digits of the input number,
+# then sums up the minimum pairs formed 
+# by iterating through the sorted digits
+# from both ends towards the middle of the list.
 
 
+
+
+print(Solution.minimumSum(Solution,'2927'))
+        
 
 
 
