7a3bb35d7a54ab63f1dadd4d23621f581045e1a4
CHANHO <chanhokim9848@gmail.com>
2024-04-16 16:11:15 +0900

    16/04/2024

--- Greedy/Remove Digit From Number to Maximize Result.py
+++ Greedy/Remove Digit From Number to Maximize Result.py
@@ -14,3 +14,23 @@ class Solution(object):
         return max(res)
     
 print(Solution.removeDigit(Solution,"1231","1"))
+
+
+# Initialization: res = [] initializes an empty list called res.
+# This list will store all numbers after removing the specified digit.
+
+# Iteration: for i in range(len(number)):
+# iterates over each index of the input number.
+
+# Condition: if number[i] == digit: 
+# checks if the current digit in the number matches the digit to be removed.
+
+# Digit Removal: res.append(number[:i] + number[i+1:])
+# removes the digit by concatenating the substring 
+# before the digit (number[:i]) with the substring 
+# after the digit (number[i+1:]), effectively removing the digit at index i.
+
+# Return Maximum: return max(res) returns the maximum number from 
+# the list of numbers after removing the digit. 
+# Since res contains numbers as strings, 
+# the max function returns the maximum string based on lexicographical order.
