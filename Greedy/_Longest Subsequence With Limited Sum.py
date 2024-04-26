9a47b15333b1e7e24e9b717062d111b385779b39
CHANHO <chanhokim9848@gmail.com>
2024-04-26 20:49:29 +0900

    26/04/2024

--- Greedy/_Longest Subsequence With Limited Sum.py
+++ Greedy/_Longest Subsequence With Limited Sum.py
@@ -1,2 +1,9 @@
-nums=sorted(accumulate(nums))
-return sorted(bisect_right(nums,q) for q in queries)
+from itertools import accumulate
+from bisect import bisect_right
+class Solution(object):
+
+    def answerQueries(self, nums,queries):
+        nums = list(accumulate(sorted(nums)))
+        return [bisect_right(nums, q) for q in queries]
+        
+print(Solution.answerQueries(Solution,[1,3,5,7,9],[3,4,5]))
