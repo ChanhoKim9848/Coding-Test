14fd2e0d41bc98f828154c97f4472c5521039de8
CHANHO <chanhokim9848@gmail.com>
2024-05-11 21:54:46 +0900

    11/05/2024

--- Greedy/_Latest Time by Replacing Hidden Digits.py
+++ Greedy/_Latest Time by Replacing Hidden Digits.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def maximumTime(self, time):
         time=list(time)
-        for i in range(len(time)):
-            if time[0]=='?': time[0]='2' if time[1]<'4' or time[1]=='?' else '1'
-            if time[1]=='?': time[1]='3' if time[0]=='2' else '9'
-            if time[3]=='?': time[3]='5'
-            if time[4]=='?': time[4]='9'
+        if time[0]=='?': time[0]='2' if time[1]<'4' or time[1]=='?' else '1'
+        if time[1]=='?': time[1]='3' if time[0]=='2' else '9'
+        if time[3]=='?': time[3]='5'
+        if time[4]=='?': time[4]='9'
         return ''.join(time)
     
 
