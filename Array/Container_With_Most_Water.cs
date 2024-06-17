a23f266003e2ffa951eb3e7abb4c9446fe17be52
ChanhoKim <chanhokim9848@gmail.com>
2024-06-17 12:41:13 +0100

    17/06/2024

--- Array/Container_With_Most_Water.cs
+++ Array/Container_With_Most_Water.cs
@@ -7,8 +7,7 @@ public class Solution {
             int value = (right-left)*Math.Min(height[left],height[right]);
             if (max<value){
                 max=value;
-            }
-
+            
             // when left and right go inward of the array
             // we want to keep the edge height as high as possible
             // so we compare two heights and move the shortest one inward first 
