e34724348c92e2fa914d3420db4665de06fa36da
ChanhoKim <chanhokim9848@gmail.com>
2024-06-28 19:53:44 +0100

    28/06/24

--- Array/_Maximum Product SubArray.py
+++ Array/_Maximum Product SubArray.py
@@ -1,7 +1,14 @@
-class Solution(object):
-    def maxProduct(self, A):
-        B = A[::-1]
-        for i in range(1, len(A)):
-            A[i] *= A[i - 1] or 1
-            B[i] *= B[i - 1] or 1
-        return max(A + B)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res=max(nums)
+        curMax=curMin=1
+        for n in nums:
+            if n==0:
+                curMin=curMax=1
+                continue
+            tmp=curMax*n
+            curMax=max(n, n*curMax, n*curMin)
+            curMin=min(n, n*curMin, tmp)
+            res=max(res, curMax)
+        return res
+
