7965f72f5ce04c1ee95380b71ba91fe665701e84
ChanhoKim <chanhokim9848@gmail.com>
2024-06-30 21:03:59 +0100

    30/06/2024

---
+++ Array/_Is_Anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # return Counter(s) == Counter(t)
+
+        if len(s)!=len(t):
+            return False
+        
+        countS, countT = {},{}
+        for i in range(len(s)):
+            countS[s[i]]=1+countS.get(s[i],0)
+            countT[t[i]]=1+countT.get(t[i],0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
+    
+    # we iterate two strings and count each character
+    # and we compare if one string has exactly same characters as the other one. return True
+    # we can use hashmap to count the number of each character of the string
+    # and we compare by iterating hashmap if it is not the same number then it return false
+    # time complexity O(S+T), we iterate through two strings, 
+    # space complexity O(S+T), as we make two hashmaps
+    
