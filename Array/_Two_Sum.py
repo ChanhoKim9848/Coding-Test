c331b36d04b36b8e0ab3ea2ea793f423d005119e
ChanhoKim <chanhokim9848@gmail.com>
2024-07-15 12:27:39 +0100

    TwoSum - 15/07/24

--- Array/_Two_Sum.py
+++ Array/_Two_Sum.py
@@ -20,6 +20,15 @@ class Solution(object):
         return []
     
 
+    def twoSum(self, nums, target):
+        h={}
+        for index,number in enumerate(nums):
+            dif=target-number
+            if dif in h:
+                return [h[dif],index]
+            h[number]=index
+        return
+
 
 
 
