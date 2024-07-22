6814760b75b2052f536fff19883616db3c4789ad
ChanhoKim <chanhokim9848@gmail.com>
2024-07-22 15:50:20 +0100

    22/07/24

--- Array/_Two_Sum.py
+++ Array/_Two_Sum.py
@@ -1,28 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-
-        # create an empty hash table to store elements and their indices
-        # iterate through the array from left to right
-        # for each element of nums[i], calculate the complement by 
-        # subtracting it from the target
-        # check if the complement exists in the hash table,
-        # we found a solution if it has
-        # if no solution found then return empty array
-        hash={}
-        n=len(nums)
-        for i in range(n):
-            hash[nums[i]]=i
-        
-        for i in range(n):
-            complement = target -nums[i]
-            if hash[complement] and hash[complement]!=i:
-                return [i,hash[complement]]
-        return []
-    
-
-
-
-    def twoSum(self, nums, target):
         h={}
         for index,number in enumerate(nums):
             dif=target-number
