f179ebcd57208784a069a24b3a36b36f068134c9
ChanhoKim <chanhokim9848@gmail.com>
2024-10-21 14:43:21 +0100

    21/10/24

---
+++ LeetCode/Array/_Greatest_Common_Divisor_of_Strings.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        len1 = len(str1)
+        len2 = len(str2)
+
+        def isDivisor(i):
+            # Check if both string lengths are divisible by i
+            if len1 % i != 0 or len2 % i != 0:
+                return False
+            
+            # Calculate the number of repetitions for the substring
+            f1, f2 = len1 // i, len2 // i
+            
+            # Verify if the substring of length i can construct both strings
+            return f1 * str1[:i] == str1 and f2 * str1[:i] == str2
+
+        # Iterate from the length of the shorter string down to 1
+        for i in range(min(len1, len2), 0, -1):
+            if isDivisor(i):
+                return str1[:i]  # Return the largest common divisor string
+
+        return ""
+
+# Initialization: The lengths of str1 and str2 are stored in len1 and len2.
+
+# Divisor Check Function: The isDivisor(i) function checks if a substring of length i can evenly divide both strings. 
+# It first verifies that both lengths are divisible by i. 
+# Then, it checks if repeating the substring str1[:i] the appropriate number of times reconstructs both strings.
+
+# Finding the GCD of Strings: The main loop iterates from the length of the shorter string down to 1
+# For each length i, it calls isDivisor(i). 
+# If a divisor is found, it returns the corresponding substring from str1.
+
+# Return Statement:
+#  If no common divisor string is found, it returns an empty string.
--- Readme.md
+++ Readme.md
@@ -6,9 +6,9 @@ deleting first commit: "git update-ref -d HEAD"
 ![Image](image.png)
 
 # LeetCode
-##### Merge Strings Alternately (14/10/24) - X O
-##### Greatest Common Divisor of Strings (15/10/24) - X X
-##### Kids with the Greatest Number of Candies (16/10/24) - X
+##### Merge Strings Alternately (21/10/24) - X O O
+##### Greatest Common Divisor of Strings (21/10/24) - X X O
+##### Kids with the Greatest Number of Candies (16/10/24) - X O
 ##### Can Place Flowers (16/10/24) - X
 ##### Reverse Vowels of a String (17/10/24) - X
 ##### Reverse Words in a String (18/10/24) - X
